97e11446e59ce0029ff53c62a06b9bed3fe19691
KorNito <kornelijuspacevicius@gmail.com>
2020-05-06 22:15:15 +0300

    Refactored App.js code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 
-// Pages
 import LoginPage from './pages/LoginPage/LoginPage';
 import Dashboard from './pages/Dashboard/Dashboard'
 
-// Test components
-import GetList from './components/GetList';
-import PostList from './components/PostList';
-
-
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
-// Main css
 import './App.css';
 
 class App extends Component {
@@ -32,61 +25,3 @@ class App extends Component {
 
 export default App;
 
-// import React from "react";
-
-// import Dashboard from './pages/Dashboard/Dashboard';
-
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
-
-// export default function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/dashboard">Dashboard</Link>
-//             </li>
-//             <li>
-//               <Link to="/about">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/users">Users</Link>
-//             </li>
-//           </ul>
-//         </nav>
-
-//         {/* A <Switch> looks through its children <Route>s and
-//             renders the first one that matches the current URL. */}
-//         <Switch>
-//           <Route path="/dashboard" component={Dashboard}>
-            
-//           </Route>
-//           <Route path="/users">
-//             <Users />
-//           </Route>
-//           <Route path="/">
-//             <Home />
-//           </Route>
-//         </Switch>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
