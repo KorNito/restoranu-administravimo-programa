9b0fadfbf032d1784413a7496e94cc4609f16ef9
KorNito <kornelijuspacevicius@gmail.com>
2020-05-06 22:59:01 +0300

    Updates

---
+++ precache-manifest.8c794a319acddd14e1eadb7fbeb6f505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cddea3c3f0749160030db44643b24a",
+    "url": "/Restoranu-administravimo-programa/index.html"
+  },
+  {
+    "revision": "951f2381237fc77422f6",
+    "url": "/Restoranu-administravimo-programa/static/css/main.3627852d.chunk.css"
+  },
+  {
+    "revision": "3ca71dabaac48e9da3d3",
+    "url": "/Restoranu-administravimo-programa/static/js/2.02dd65a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Restoranu-administravimo-programa/static/js/2.02dd65a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951f2381237fc77422f6",
+    "url": "/Restoranu-administravimo-programa/static/js/main.130d8418.chunk.js"
+  },
+  {
+    "revision": "83cdbddbc32783ff52c6",
+    "url": "/Restoranu-administravimo-programa/static/js/runtime-main.dffeabd7.js"
+  }
+]);
